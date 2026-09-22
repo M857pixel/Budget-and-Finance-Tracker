86def78cb78053dfb3d30bd65af0572a332458e9
Jordan-Hartman04 <jhartman5333@gmail.com>
2026-09-21 20:11:08 -0500

    Changed Variable Names for Consistency - Jordan Hartman

--- supabase/migrations/202609212000_schema.sql
+++ supabase/migrations/202609212000_schema.sql
@@ -8,11 +8,11 @@
  Jordan Hartman
  */
 
- -- Can be acessed via user_id
+-- Can be acessed via user_id
 CREATE TABLE IF NOT EXISTS users (
     user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    password_hash VARCHAR(100) NOT NULL --Called password_hash, will be a password until we have encryption set up
+    user_email VARCHAR(100) NOT NULL,
+    user_password_hash VARCHAR(100) NOT NULL --Called password_hash, will be a password until we have encryption set up
     );
 
 -- Can be accessed via transaction_id and user_id
@@ -20,11 +20,11 @@ CREATE TABLE IF NOT EXISTS transactions (
     transaction_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
 
-    name VARCHAR(50) NOT NULL,
+    transaction_name VARCHAR(50) NOT NULL,
     transaction_type VARCHAR(50),
-    amount DECIMAL(12, 2) NOT NULL,
+    transaction_amount DECIMAL(12, 2) NOT NULL,
     transaction_date DATE NOT NULL,
-    description VARCHAR(250),
+    transaction_description VARCHAR(250),
 
     FOREIGN KEY (user_id)
     REFERENCES users(user_id)
