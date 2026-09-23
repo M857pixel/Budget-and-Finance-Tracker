920d75aa460e1a3abfb87d274286a65dbad57084
elevenponcho <benthurman17@gmail.com>
2026-09-22 23:16:15 -0500

    Mistrial AI API Test

---
+++ api/mistralAPI.sh
@@ -0,0 +1,14 @@
+export MISTRAL_API_KEY="insert-api-key-here"
+
+curl https://api.mistral.ai/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $MISTRAL_API_KEY" \
+  -d '{
+    "model": "ministral-8b-latest",
+    "messages": [
+      {
+        "role": "user",
+        "content": "You are going to select a command for a database output of the following. You must simply select the command to use and nothing else. Select from the following for the given prompt: Select * FROM Food Select * FROM Birthday Select * FROM Tax. The prompt is How much have I spent on food"
+      }
+    ]
+  }'
